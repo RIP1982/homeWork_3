21e1b3de426cdc27eae31442dae519f6830b7495
RIP1982 <perovsky@list.ru>
2022-06-15 20:23:44 +0400

    Cube between 1 to N

---
+++ Task_23/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 23: Напишите программу, которая принимает на 
+// вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27.
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+int number = Input("Введите число N: ");
+double i = 2;
+//for(i <= number; Math.Pow(i, 3); i++)
+
+while(i <= number)
+{
+    double pow = Math.Pow(i, 3);
+    Console.WriteLine($"{i} x {i} x {i} = {pow}");
+    Console.WriteLine();
+    i = i + 1;
+}
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
